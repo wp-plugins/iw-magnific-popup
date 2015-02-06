6212793edbfb40fe24b5ef92d1b8a0032d501b2e
welshhuw <welshhuw@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-06 14:18:37 +0000

    Added icons.
    git-svn-id: https://plugins.svn.wordpress.org/iw-magnific-popup/tags/1.2.4@1083839 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/includes/scripts.php
@@ -0,0 +1,97 @@
+<?php
+
+/*******************
+	Load JS Scripts & CSS Styles
+*******************/
+
+// Load the Javscript and CSS files.
+function iwmp_load_styles() {
+
+	wp_enqueue_style( 'iwmp-styles', plugins_url( '/assets/magnific-popup.css', __FILE__ ) );
+	
+	// Load JS in footer
+	wp_enqueue_script( 'iwmp-scripts', plugins_url( '/assets/jquery.magnific-popup.min.js', __FILE__ ), array( 'jquery' ), '1.0', true );
+	
+}
+
+add_action( 'wp_enqueue_scripts', 'iwmp_load_styles' );
+	
+//Add script to the footer, to initate Magnific Popup.
+function iwmp_add_script() { ?>
+
+	<script>	
+		// Ref: http://ajtroxell.com/use-magnific-popup-with-wordpress-now/
+		jQuery(document).ready(function($) {
+			// Single Image
+			$('a[href*=".jpg"], a[href*=".jpeg"], a[href*=".png"], a[href*=".gif"]').each(function(){
+				//single image popup
+				if ($(this).parents('.iwmp-gallery').length == 0) {
+					$(this).magnificPopup({
+						type:'image',
+						callbacks: {
+							open: function() {
+				        $('.mfp-description').append(this.currItem.el.attr('alt'));
+				      },
+				      afterChange: function() {
+				        $('.mfp-description').empty().append(this.currItem.el.attr('alt'));
+				      }
+				    },
+						image: {
+							markup: 
+							'<div class="mfp-figure">'+
+							'<div class="mfp-close"></div>'+
+							'<div class="mfp-img"></div>'+
+							'<div class="mfp-bottom-bar">'+
+							'<div class="mfp-title"></div>'+
+							'<div class="mfp-description"></div>'+
+							'<div class="mfp-counter"></div>'+
+							'</div>'+
+							'</div>',
+							titleSrc: function(item) {
+								return item.el.find('img').attr('alt');
+							}							
+						}
+					});
+				}
+			});		
+				
+			// Gallery Images
+			$('.iwmp-gallery, .woocommerce-main-image').each(function() {
+				$(this).magnificPopup({
+					delegate: 'a[href*=".jpg"], a[href*=".jpeg"], a[href*=".png"], a[href*=".gif"]',
+					type: 'image',
+					gallery: {enabled:true},
+					callbacks: {
+						open: function() {
+			        $('.mfp-description').append(this.currItem.el.attr('alt'));
+			      },
+			      afterChange: function() {
+			        $('.mfp-description').empty().append(this.currItem.el.attr('alt'));
+			      }
+			    },
+					image: {
+						markup: 
+						'<div class="mfp-figure">'+
+						'<div class="mfp-close"></div>'+
+						'<div class="mfp-img"></div>'+
+						'<div class="mfp-bottom-bar">'+
+						'<div class="mfp-title"></div>'+
+						'<div class="mfp-description"></div>'+
+						'<div class="mfp-counter"></div>'+
+						'</div>'+
+						'</div>',
+						titleSrc: function(item) {
+							return item.el.find('img').attr('alt');
+						}
+					},
+				});
+			}); 
+		});		
+		
+		</script>
+		
+<?php	}
+
+add_action( 'wp_footer', 'iwmp_add_script' );
+
+?>
---
+++ trunk/readme.txt
@@ -0,0 +1,47 @@
+=== IW Magnific Popup ===
+Contributors: welshhuw
+Tags: responsive, images, lightbox, gallery
+Requires at least: 3.9
+Tested up to: 4.1
+Stable tag: 1.2.4
+License: GPLv3
+License URI: https://www.gnu.org/copyleft/gpl.html
+
+A plugin to include the Magnific Popup lightbox for single & gallery images in WordPress.
+
+== Description ==
+
+This simple plugin has no options (yet), install, activate and forget. It currently support single images in posts and pages, and gallery images using the built-in WordPress Gallery shortcode. 
+Images within a Gallery will have next and previous arrows.
+You can add an alt attribute to your image, to enable lightbox 'captions'.
+
+Thanks to Dmitry Semenov for the original script and hard work: http://dimsemenov.com/plugins/magnific-popup/
+
+Note: This only supports images and not the full capabilities of Magnific Popup.
+
+== Installation ==
+
+Upload the plugin into your plugins folder, activate and enjoy.
+
+== Changelog ==
+
+= 1.2.4 =
+* Added icons
+
+= 1.2.3 =
+* Small fix to stop popup opening on WooCommerce product images (temp fix).
+
+= 1.2.2 =
+* Small fix where next & prev arrows had theme based colours on hover, now removed.
+
+= 1.2.1 =
+* Added Alt attribute functionality.
+
+= 1.1.1 =
+* Small Fix.
+
+= 1.1.0 =
+* Added .iwmp-gallery class to the main gallery div wrapper to avoid conflicts.
+
+= 1.0.0 =
+* Initial release.
